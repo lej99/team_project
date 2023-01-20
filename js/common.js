0e476e9a00da58e5b6d9f94ec70e4900e35056ed
lej99 <eunji_ouo@naver.com>
2023-01-20 17:44:28 +0900

    23.01.20(0)

--- js/common.js
+++ js/common.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
+
     $(".menu_icon").click(function () {
         $(this).toggleClass("active")
         $('.header_menu').toggleClass('right');
     });
 
     $('.footer_menu_wrap').hide();
+    
     $(".menu_button").click(function () {
         $('.close').toggleClass("none")
         $('.open').toggleClass("none")
--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+    $('#gallery').addClass("active")
+    $('#gallery').siblings().removeClass('active');
+    
     $('#fade_wrap>.fade.two').animate({width:0},800);
     $('#fade_wrap').delay(1200).animate({width:0},800);
 });
