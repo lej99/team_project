03f1be0365091d17944e11e3a4263e168b1d0d32
lej99 <eunji_ouo@naver.com>
2023-01-26 17:56:08 +0900

    23.01.26(0)

--- js/gallery.js
+++ js/gallery.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('#gallery').addClass("active")
     $('#gallery').siblings().removeClass('active');
     
-    $('#fade_wrap>.fade.two').animate({width:0},800);
-    $('#fade_wrap').delay(1200).animate({width:0},800);
+    $('#fade_wrap>.fade.two').animate({width:0});
+    $('#fade_wrap').delay(600).animate({width:0}); 
+
 });
