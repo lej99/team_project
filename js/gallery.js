bc7dc551d5982895cdaa290f204a973a1c4bdf91
lej99 <eunji_ouo@naver.com>
2023-01-30 18:16:17 +0900

    23.01.30(0)

--- js/gallery.js
+++ js/gallery.js
@@ -2,7 +2,5 @@ $(document).ready(function(){
     $('#gallery').addClass("active")
     $('#gallery').siblings().removeClass('active');
     
-    $('#fade_wrap>.fade.two').animate({width:0},650);
-    $('#fade_wrap').delay(650).animate({width:0},650); 
-
+    $('#fade_wrap').animate({width:0},650); 
 });
--- js/production.js
+++ js/production.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
     $('#production').addClass("active")
     $('#production').siblings().removeClass('active');
-    
-    $('#fade_wrap>.fade.two').animate({width:0},650);
-    $('#fade_wrap').delay(650).animate({width:0},650); 
 
+    $('#fade_wrap').animate({width:0},650); 
 });
