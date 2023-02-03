65b173a3914a9be63c9b4b79589e32e39e1240f1
lej99 <eunji_ouo@naver.com>
2023-02-03 10:25:25 +0900

    23.02.03(0)
    gallery modal

--- js/gallery.js
+++ js/gallery.js
@@ -3,4 +3,16 @@ $(document).ready(function(){
     $('#gallery').siblings().removeClass('active');
     
     $('#fade_wrap').animate({width:0},650); 
+
+    $('.swiper-slide img').click(function(){
+        let src = $(this).attr('src');
+        let alt = $(this).attr('alt');
+        $('.modal').fadeIn();
+        $('.modal img').attr('src',src);
+        $('.modal img').attr('alt',alt);
+    });
+
+    $('.close').click(function(){
+        $('.modal').fadeOut();
+    });
 });
